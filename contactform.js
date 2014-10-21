00f52a95df04e45115a5585bab10d2a6a934f9da
Andrew Anderson <gimlilongbow@gmail.com>
2014-10-20 21:42:07 -0400

    Initialize files.

---
+++ contactform.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+	
+	//if submit button is clicked
+	$('#submit').click(function () {		
+		
+		//Get the data from all the fields
+		var name = $('input[name=name]');
+		var email = $('input[name=email]');
+		var website = $('input[name=website]');
+		var comment = $('textarea[name=comment]');
+
+		//Simple validation to make sure user entered something
+		//If error found, add hightlight class to the text field
+		if (name.val()=='') {
+			name.addClass('hightlight');
+			return false;
+		} else name.removeClass('hightlight');
+		
+		if (email.val()=='') {
+			email.addClass('hightlight');
+			return false;
+		} else email.removeClass('hightlight');
+		
+		if (comment.val()=='') {
+			comment.addClass('hightlight');
+			return false;
+		} else comment.removeClass('hightlight');
+
+		// Disable the submit button.
+		$('#submit').attr('disabled','disabled');
+
+		
+		//organize the data properly
+		var data = 'name=' + name.val() + '&email=' + email.val() + '&website=' + 
+		website.val() + '&comment='  + encodeURIComponent(comment.val());
+		
+		//disabled all the text fields
+		$('.text').attr('disabled','true');
+		
+		//show the loading sign
+		$('.loading').show();
+		
+		//start the ajax
+		$.ajax({
+			//this is the php file that processes the data and send mail
+			url: "process.php",	
+			
+			//GET method is used
+			type: "GET",
+
+			//pass the data			
+			data: data,		
+			
+			//Do not cache the page
+			cache: false,
+			
+			//success
+			success: function (html) {				
+				//if process.php returned 1/true (send mail success)
+				if (html==1) {					
+					//hide the form
+					$('.rsvpForm').slideUp('slow');					
+					
+					//show the success message
+					$('.done').slideDown('slow');
+					
+				//if process.php returned 0/false (send mail failed)
+				} else {
+					$('.loading').hide();
+					$('#submit').removeAttr('disabled');
+					alert('Sorry, unexpected error. Please try again later.');
+				}
+			}		
+		});
+		
+		//cancel the submit button default behaviours
+		return false;
+	});	
+});	
